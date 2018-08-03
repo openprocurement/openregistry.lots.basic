91e970248f055afb36c46e41ce60d60717ad5d27
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-08-03 12:48:05 +0300

    Add assets field

--- openregistry/lots/basic/models.py
+++ openregistry/lots/basic/models.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
-from schematics.types import StringType
+from schematics.types import (
+    StringType,
+    MD5Type,
+)
+from schematics.types.compound import ListType
 from zope.interface import implementer
 
-from openregistry.lots.core.models import ILot, Lot as BaseLot
+from openregistry.lots.core.models import (
+    ILot,
+    Lot as BaseLot,
+    validate_asset_uniq
+)
 
 
 class IBasicLot(ILot):
@@ -13,4 +21,7 @@ class IBasicLot(ILot):
 class Lot(BaseLot):
     lotType = StringType(default="basic")
     lotIdentifier = StringType(required=True, min_length=1)
+    assets = ListType(MD5Type(), required=True, min_size=1,
+                      validators=[validate_asset_uniq])
+
     _internal_type = 'basic'
